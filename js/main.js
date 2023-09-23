1da76de8b715844b5eb7d3d4076f04868ea10f56
vincenzo2202 <vincenzodonnarumma22@gmail.com>
2023-09-24 01:17:44 +0200

    Se le agrega interación a los botones del lado derecho para que muestre un imagen diferente cada uno usando javascript

---
+++ js/main.js
@@ -0,0 +1,19 @@
+let pantalla = document.getElementById("screen");
+
+
+
+
+document.addEventListener("click", (e)=>{
+    if(e.target.id=== 'Accion1' || e.target.class === 'circleTop' ){
+        pantalla.style.backgroundImage = "url('../img/LOL.jpg')";
+
+    }else if (e.target.id === 'Accion2'){
+        pantalla.style.backgroundImage = "url('../img/CRASH.jpg')";
+    }else if (e.target.id === 'Accion3'){
+        pantalla.style.backgroundImage = "url('../img/GTA.jpg')";
+    }else if (e.target.id === 'Accion4'){
+        pantalla.style.backgroundImage = "url('../img/MARIO.jpg')";}
+});
+
+ 
+

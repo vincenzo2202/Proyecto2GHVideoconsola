ad693a969b04205e67ca916f0629cebf9955dee7
vincenzo2202 <vincenzodonnarumma22@gmail.com>
2023-09-24 09:49:08 +0200

    Funcionalidad al boton de apagado y reset

--- js/main.js
+++ js/main.js
@@ -1,16 +1,35 @@
 let pantalla = document.getElementById("screen");
 
-document.addEventListener("click", (e)=>{
-    if(e.target.id=== 'Accion1' || e.target.class === 'circleTop' ){
-        pantalla.style.backgroundImage = "url('../img/LOL.jpg')";
+document.addEventListener("click", (e) => {
 
-    }else if (e.target.id === 'Accion2'){
-        pantalla.style.backgroundImage = "url('../img/CRASH.jpg')";
-    }else if (e.target.id === 'Accion3'){
-        pantalla.style.backgroundImage = "url('../img/GTA.jpg')";
-    }else if (e.target.id === 'Accion4'){
-        pantalla.style.backgroundImage = "url('../img/MARIO.jpg')";}
+    if (e.target.id === 'start') {
+
+        pantalla.style.backgroundImage = "url('../img/play1gif2.gif')";
+        
+        
+        document.addEventListener("click", (e) => {
+
+            if (e.target.id === 'Accion1') {
+                pantalla.style.backgroundImage = "url('../img/LOL.jpg')";
+
+            } else if (e.target.id === 'Accion2') {
+                pantalla.style.backgroundImage = "url('../img/CRASH.jpg')";
+            } else if (e.target.id === 'Accion3') {
+                pantalla.style.backgroundImage = "url('../img/GTA.jpg')";
+            } else if (e.target.id === 'Accion4') {
+                pantalla.style.backgroundImage = "url('../img/MARIO.jpg')";
+            } else if (e.target.id === 'reset') {
+                pantalla.style.backgroundImage = "none"
+            }
+
+        });
+    } else {
+
+        pantalla.style.backgroundImage = pantalla
+
+    };
 });
 
- 
+
+
 
